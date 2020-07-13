eeb7739c22306aa7f1edcaae7b6deb4144691d56
Di Liu <66962966+diliu25@users.noreply.github.com>
2020-07-13 01:26:50 -0400

    update main.py
    add documentation and example run for fixed_seats_model_with_radius

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from fixed_seats_model import fixed_seats_model
 from plot_results import plot_layout_result
+from fixed_seats_model_with_radius import fixed_seats_model_with_radius
 
 ############### Function documentation ##############
 ## fixed_seats_model(data_file, d0, d1 = 0, d2 = 0, d3 = 0, firstrow: bool = False, firstrow_y = 0.0,
@@ -30,11 +31,32 @@ from plot_results import plot_layout_result
 # firstrow_y: float, y-coordinate of firstrow. If 0, the first row is not treated as empty. Defaul is 0.
 ## Output: a pdf file of classroom layout plot.
 
+## fixed_seats_model_with_radius(data_file, seat_radius, d0, d1 = 0, d2 = 0, d3 = 0, firstrow: bool = False, firstrow_y = 0.0, type=1, num_selection = 10, output_filename_affix='output'):
+# Create and solve IP model for selecting seats among a given set of fixed seats in a classroom while considering the physical space each seat occupies.
+## Parameters:
+# data_file: str, coordinate file name of .csv or .xlsx file with 3 columns: Feature, X, Y.
+# seat_radius: float, distance from center to side of seat space (assumed as a square).
+# d0: float or int, social distance (inches) between seats.
+# d1: float or int, social distance (inches) between seats and instructor. Default is 0.
+# d2: float or int, safety distance (inches) from doors. Default is 0.
+# d3: float or int, safety distance (inches) from aisles. Default is 0.
+# firstrow: bool, False means incorporting seats of the first row, True means eliminating seats of the first row. Default is False.
+# firstrow_y: float or int, the y-coordinate of seats in the first row. Default is 0. Required if firstrow is True
+# type: int {1, 2, 3, 4},
+#     if type=1: maximize number of selected seats under specified distance and prevention constraints.
+#     if type=2: maximize total distance while selecting specified number of students.
+#     if type=3: maximize minimum distance while selecting specified number of students.
+#     if type=4: minimize number of pairs of adjacent seats (next to each other in the same row).
+#     Default is 1.
+# num_selection: int, the number of selected seats, required if type is 2, 3, or 4. Default is 10.
+# output_filename_affix: str, adding to the end of input filename as output filename.
+## Output: append coordinates of selected seats at the end of input data_file and create an output file.
+
 
 ############### Example run ######################
 
 ## change to your path to input file. This example excel file can be found in examples directory
-file = "example_all_coordinates.xlsx"  # input file name, required parameter
+file = "input/example_all_coordinates.xlsx"  # input file name, required parameter
 
 ## other parameters:
 social_distance = 6 * 12  # inches, required parameter
@@ -49,7 +71,14 @@ fixed_seats_model(file, d0=social_distance, d2=door_distance, d3=aisle_distance,
                   output_filename_affix=output_filename_affix)
 
 ## change to your path to output file. This example excel file can be found in examples directory
-result_file = "example_all_coordinates_6ft_prevention.xlsx"  # file to be plotted
+result_file = "input/example_all_coordinates_6ft_prevention.xlsx"  # file to be plotted
 
 ## this line will creat a pdf file of classroom layout plot
 plot_layout_result(result_file, firstrow_y=firstrowy)
+
+## change to your path to input file. This example excel file can be found in examples directory
+file = "input/example_all_coordinates.xlsx"
+
+## this line will create an output file: example_all_coordinates_6ft_radius.xlsx
+fixed_seats_model_with_radius(file, seat_radius=12, d0=6 * 12, output_filename_affix='6ft_radius')
+
